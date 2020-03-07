eb2d99cd42f21c63b7e7484540567f4c6c5c1e79
Matthew <justmatt@outlook.com>
2020-03-07 14:27:36 -0600

    Added settings for stashing items

--- MegaStorage/Framework/Models/ModConfig.cs
+++ MegaStorage/Framework/Models/ModConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using StardewModdingAPI;
 using SObject = StardewValley.Object;
 
 namespace MegaStorage.Framework.Models
@@ -7,6 +11,31 @@ namespace MegaStorage.Framework.Models
     {
         public bool EnableChest { get; set; }
         public bool EnableCategories { get; set; }
+    }
+
+    public class StashConfig
+    {
+        public string Includes { get; set; } = "";
+        public string Excludes { get; set; } = "";
+
+        internal IList<int> IncludesAsList => !string.IsNullOrWhiteSpace(Includes)
+            ? Includes
+                .Split(' ')
+                .Select(c => Convert.ToInt32(c, CultureInfo.InvariantCulture))
+                .ToList()
+            : null;
+        internal IList<int> ExcludesAsList => !string.IsNullOrWhiteSpace(Excludes)
+            ? Excludes
+                .Split(' ')
+                .Select(c => Convert.ToInt32(c, CultureInfo.InvariantCulture))
+                .ToList()
+            : null;
+    }
+
+    public class CustomCategoryConfig: StashConfig
+    {
+        public string CategoryName { get; set; }
+        public string Image { get; set; } = "";
     }
 
     public class ModConfig
@@ -26,64 +55,120 @@ namespace MegaStorage.Framework.Models
             EnableChest = true,
             EnableCategories = true
         };
-        public IDictionary<string, IList<int>> Categories { get; set; } = new Dictionary<string, IList<int>>()
+
+        public SButton StashKey { get; set; } = SButton.Q;
+        public SButton StashAnywhereKey { get; set; } = SButton.Z;
+        public SButton StashButton { get; set; } = SButton.RightStick;
+
+        public IList<CustomCategoryConfig> Categories { get; set; } =
+            new List<CustomCategoryConfig>(7)
+            {
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "All",
+                        Image = "AllTab.png"
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Crops",
+                        Includes =
+                            SObject.GreensCategory + " " +
+                            SObject.flowersCategory + " " +
+                            SObject.FruitsCategory + " " +
+                            SObject.VegetableCategory
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Seeds",
+                        Includes =
+                            SObject.SeedsCategory + " " +
+                            SObject.fertilizerCategory
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Materials",
+                        Includes =
+                            SObject.metalResources + " " +
+                            SObject.buildingResources + " " +
+                            SObject.GemCategory + " " +
+                            SObject.mineralsCategory + " " +
+                            SObject.CraftingCategory + " " +
+                            SObject.monsterLootCategory
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Cooking",
+                        Includes =
+                            SObject.ingredientsCategory + " " +
+                            SObject.CookingCategory + " " +
+                            SObject.sellAtPierresAndMarnies + " " +
+                            SObject.meatCategory + " " +
+                            SObject.MilkCategory + " " +
+                            SObject.EggCategory + " " +
+                            SObject.syrupCategory + " " +
+                            SObject.artisanGoodsCategory
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Fishing",
+                        Includes =
+                            SObject.FishCategory + " " +
+                            SObject.baitCategory + " " +
+                            SObject.tackleCategory
+                    }
+                },
+                {
+                    new CustomCategoryConfig()
+                    {
+                        CategoryName = "Misc",
+                        Includes =
+                            SObject.furnitureCategory + " " +
+                            SObject.junkCategory
+                    }
+                }
+            };
+
+        public StashConfig StashItems { get; set; } = new StashConfig()
         {
-            {
-                "Crops", new List<int>()
-                {
-                    SObject.GreensCategory,
-                    SObject.flowersCategory,
-                    SObject.FruitsCategory,
-                    SObject.VegetableCategory
-                }
-            },
-            {
-                "Seeds", new List<int>()
-                {
-                    SObject.SeedsCategory,
-                    SObject.fertilizerCategory
-                }
-            },
-            {
-                "Materials", new List<int>()
-                {
-                    SObject.metalResources,
-                    SObject.buildingResources,
-                    SObject.GemCategory,
-                    SObject.mineralsCategory,
-                    SObject.CraftingCategory,
-                    SObject.monsterLootCategory
-                }
-            },
-            {
-                "Cooking", new List<int>()
-                {
-                    SObject.ingredientsCategory,
-                    SObject.CookingCategory,
-                    SObject.sellAtPierresAndMarnies,
-                    SObject.meatCategory,
-                    SObject.MilkCategory,
-                    SObject.EggCategory,
-                    SObject.syrupCategory,
-                    SObject.artisanGoodsCategory
-                }
-            },
-            {
-                "Fishing", new List<int>()
-                {
-                    SObject.FishCategory,
-                    SObject.baitCategory,
-                    SObject.tackleCategory
-                }
-            },
-            {
-                "Misc", new List<int>()
-                {
-                    SObject.furnitureCategory,
-                    SObject.junkCategory
-                }
-            }
+            Includes =
+                SObject.GreensCategory + " " +
+                SObject.flowersCategory + " " +
+                SObject.FruitsCategory + " " +
+                SObject.VegetableCategory + " " +
+                SObject.SeedsCategory + " " +
+                SObject.fertilizerCategory + " " +
+                SObject.metalResources + " " +
+                SObject.buildingResources + " " +
+                SObject.GemCategory + " " +
+                SObject.mineralsCategory + " " +
+                SObject.CraftingCategory + " " +
+                SObject.monsterLootCategory + " " +
+                SObject.ingredientsCategory + " " +
+                SObject.CookingCategory + " " +
+                SObject.sellAtPierresAndMarnies + " " +
+                SObject.meatCategory + " " +
+                SObject.MilkCategory + " " +
+                SObject.EggCategory + " " +
+                SObject.syrupCategory + " " +
+                SObject.artisanGoodsCategory + " " +
+                SObject.FishCategory + " " +
+                SObject.baitCategory + " " +
+                SObject.tackleCategory + " " +
+                SObject.furnitureCategory + " " +
+                SObject.junkCategory
         };
+
         public ModConfig()
         {
             Instance = this;
