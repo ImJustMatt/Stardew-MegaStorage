02111ed590c213f6e58ec1b910ec5206870ada86
Matthew <justmatt@outlook.com>
2020-03-08 01:06:31 -0600

    Added Checkbox as type of CustomClickableTextureComponent

---
+++ MegaStorage/Framework/UI/Widgets/Checkbox.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace MegaStorage.Framework.UI.Widgets
+{
+    internal class Checkbox : CustomClickableTextureComponent
+    {
+        /*********
+        ** Fields
+        *********/
+        public bool IsChecked;
+
+        /*********
+        ** Public methods
+        *********/
+        public Checkbox(
+            string name,
+            IClickableMenu parentMenu,
+            Vector2 offset)
+            : base(name, parentMenu, offset, Game1.mouseCursors, OptionsCheckbox.sourceRectUnchecked)
+        {
+            LeftClickAction = LeftClick;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        private void LeftClick(ClickableComponent clickableComponent = null)
+        {
+            IsChecked = !IsChecked;
+            sourceRect = IsChecked
+                ? OptionsCheckbox.sourceRectChecked
+                : OptionsCheckbox.sourceRectUnchecked;
+        }
+    }
+}
