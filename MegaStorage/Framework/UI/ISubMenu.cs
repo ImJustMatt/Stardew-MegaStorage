eebacd5077797cdfceff4d7b1136b3cf0f521994
Matthew <justmatt@outlook.com>
2020-03-08 01:07:32 -0600

    Added ISubMenu Interface

---
+++ MegaStorage/Framework/UI/ISubMenu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley.Menus;
+
+namespace MegaStorage.Framework.UI
+{
+    internal enum MenuType
+    {
+        BaseMenu = 0,
+        Overlay = 1
+    }
+
+    internal interface ISubMenu
+    {
+        IClickableMenu ParentMenu { get; }
+        MenuType MenuType { get; }
+        Rectangle Bounds { get; }
+        Vector2 Offset { get; }
+        Vector2 Position { get; }
+        Vector2 Dimensions { get; }
+        bool Visible { get; set; }
+    }
+}
